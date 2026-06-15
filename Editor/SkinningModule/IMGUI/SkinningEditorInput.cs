337fa7cfe1d09bd5a2ac3c94bee46658cd7e766b
m_fedorov <smoketlt@gmail.com>
2026-06-15 18:03:41 +0400

    Poll physical Alt key on Windows

--- Editor/SkinningModule/IMGUI/SkinningEditorInput.cs
+++ Editor/SkinningModule/IMGUI/SkinningEditorInput.cs
@@ -1,9 +1,19 @@
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 namespace UnityEditor.U2D.Animation
 {
     internal static class SkinningEditorInput
     {
+#if UNITY_EDITOR_WIN
+        private const int k_VKMenu = 0x12;
+        private const int k_VKLeftMenu = 0xA4;
+        private const int k_VKRightMenu = 0xA5;
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+#endif
+
         public static bool altKeyDown { get; private set; }
 
         public static bool Update(Event evt)
@@ -13,13 +23,31 @@ namespace UnityEditor.U2D.Animation
 
             bool previousAltKeyDown = altKeyDown;
 
+#if UNITY_EDITOR_WIN
+            altKeyDown = IsPhysicalAltKeyDown();
+#else
             if (IsAltKeyEvent(evt, EventType.KeyDown) || (evt.alt && !IsKeyEvent(evt, EventType.KeyUp)))
                 altKeyDown = true;
             else if (IsAltKeyEvent(evt, EventType.KeyUp) || IsAnyKeyUpWithoutAlt(evt))
                 altKeyDown = false;
+#endif
 
             return previousAltKeyDown != altKeyDown;
         }
+
+#if UNITY_EDITOR_WIN
+        private static bool IsPhysicalAltKeyDown()
+        {
+            return IsVirtualKeyDown(k_VKMenu) ||
+                IsVirtualKeyDown(k_VKLeftMenu) ||
+                IsVirtualKeyDown(k_VKRightMenu);
+        }
+
+        private static bool IsVirtualKeyDown(int virtualKey)
+        {
+            return (GetAsyncKeyState(virtualKey) & 0x8000) != 0;
+        }
+#endif
 
         private static bool IsAltKeyEvent(Event evt, EventType eventType)
         {
