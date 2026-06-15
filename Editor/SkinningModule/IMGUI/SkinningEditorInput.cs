437e2c420a479d7a10dec6507b2f634a9256492b
m_fedorov <smoketlt@gmail.com>
2026-06-15 17:51:58 +0400

    Ignore synthetic events when tracking Alt

--- Editor/SkinningModule/IMGUI/SkinningEditorInput.cs
+++ Editor/SkinningModule/IMGUI/SkinningEditorInput.cs
@@ -12,19 +12,41 @@ namespace UnityEditor.U2D.Animation
                 return false;
 
             bool previousAltKeyDown = altKeyDown;
-            bool currentAltKeyDown = evt.alt;
 
-            if ((evt.type == EventType.KeyDown || evt.rawType == EventType.KeyDown) &&
-                (evt.keyCode == KeyCode.LeftAlt || evt.keyCode == KeyCode.RightAlt))
-                currentAltKeyDown = true;
-
-            if ((evt.type == EventType.KeyUp || evt.rawType == EventType.KeyUp) &&
-                (evt.keyCode == KeyCode.LeftAlt || evt.keyCode == KeyCode.RightAlt))
-                currentAltKeyDown = false;
-
-            altKeyDown = currentAltKeyDown;
+            if (IsAltKeyEvent(evt, EventType.KeyDown))
+                altKeyDown = true;
+            else if (IsAltKeyEvent(evt, EventType.KeyUp))
+                altKeyDown = false;
+            else if (evt.alt)
+                altKeyDown = true;
+            else if (IsReliableModifierEvent(evt))
+                altKeyDown = false;
 
             return previousAltKeyDown != altKeyDown;
+        }
+
+        private static bool IsAltKeyEvent(Event evt, EventType eventType)
+        {
+            return (evt.type == eventType || evt.rawType == eventType) &&
+                (evt.keyCode == KeyCode.LeftAlt || evt.keyCode == KeyCode.RightAlt);
+        }
+
+        private static bool IsReliableModifierEvent(Event evt)
+        {
+            return evt.type == EventType.KeyDown ||
+                evt.type == EventType.KeyUp ||
+                evt.type == EventType.MouseDown ||
+                evt.type == EventType.MouseUp ||
+                evt.type == EventType.MouseMove ||
+                evt.type == EventType.MouseDrag ||
+                evt.type == EventType.ScrollWheel ||
+                evt.rawType == EventType.KeyDown ||
+                evt.rawType == EventType.KeyUp ||
+                evt.rawType == EventType.MouseDown ||
+                evt.rawType == EventType.MouseUp ||
+                evt.rawType == EventType.MouseMove ||
+                evt.rawType == EventType.MouseDrag ||
+                evt.rawType == EventType.ScrollWheel;
         }
 
         public static void Reset()
